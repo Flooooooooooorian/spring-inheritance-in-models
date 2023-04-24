657130a468b5f37c2d0ea8ccd2df44f77df7fefb
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-04-24 14:15:37 +0200

    Setup Spring

---
+++ src/main/java/de/neuefische/springinheritanceinmodels/SpringInheritanceInModelsApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.springinheritanceinmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInheritanceInModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInheritanceInModelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/springinheritanceinmodels/SpringInheritanceInModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.springinheritanceinmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInheritanceInModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
